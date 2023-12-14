c7018dceb00fa61cd2dd0dab4360b72c1461e312
christianburke2022 <Christian.burke@snhu.edu>
2023-12-13 21:50:21 -0500

    edit and delete linked to user

--- pages/models.py
+++ pages/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-
-
+from django.contrib.auth.models import User
+from django.conf import settings
 class Image(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField(blank=True)
     image = models.ImageField(upload_to='users/%Y/%m/%d/', blank=True)
     predicted_result = models.CharField(max_length=100, blank=True)
-
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     def __str__(self):
         return self.title
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import TemplateView, ListView
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
+from django.contrib import messages
 from .forms import ImageForm
 from .models import Image
 
@@ -14,10 +17,12 @@ class ImageListView(ListView):
     context_object_name = "images"
 
 
+@login_required
 def image_upload_view(request):
     if request.method == "POST":
         form = ImageForm(request.POST, request.FILES)
         if form.is_valid():
+            form.instance.author = request.user
             form.save()
             return redirect("image_list")
     else:
@@ -25,8 +30,16 @@ def image_upload_view(request):
     return render(request, "upload.html", {"form": form})
 
 
+@login_required
 def image_edit_view(request, image_id):
     image = get_object_or_404(Image, id=image_id)
+    try:
+        # Check if the logged in user is the author of the image
+        if image.author != request.user:
+            return HttpResponseForbidden("You don't have permission to edit this image")
+    except Image.author.RelatedObjectDoesNotExist:
+        messages.error(request, "This image has no associated author.")
+        return redirect('home')
 
     if request.method == "POST":
         form = ImageForm(request.POST, request.FILES, instance=image)
@@ -35,15 +48,23 @@ def image_edit_view(request, image_id):
             return redirect("image_list")
     else:
         form = ImageForm(instance=image)
-
     return render(request, "image_edit.html", {"form": form, "image": image})
 
 
+@login_required
 def image_delete_view(request, image_id):
     image = get_object_or_404(Image, id=image_id)
+    try:
+        # Check if the logged in user is the author of the image
+        if image.author != request.user:
+            messages.error(request, "You Dont have Permission to delete this image.")
+            return redirect('home')
+    except Image.author.RelatedObjectDoesNotExist:
+        messages.error(request, "This image has no associated author.")
+        return redirect('home')
 
     if request.method == "POST":
         image.delete()
-        return redirect("image_list")
+        return redirect('home')
 
     return render(request, "image_delete.html", {"image": image})
